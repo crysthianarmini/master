3a433c6934e6e21822c45889be812deb3c8fab66
Crysthian Armini <crysthian.armini@objective.com.br>
2022-09-22 10:57:10 -0300

    evolução exercicio 4

--- app/exercicio_4.py
+++ app/exercicio_4.py
@@ -1,12 +1,12 @@
 
 class Usuario:
-    def __init__(self,nome,cep) -> None:
+    def __init__(self,nome:str,cep:str) -> None:
         self.nome = nome
-        self.valor = cep
+        self.cep = cep
 
 class Product:
 
-    def __init__(self,nome,valor) -> None:
+    def __init__(self,nome:str,valor:float) -> None:
         self.nome = nome
         self.valor = valor
 
@@ -18,10 +18,16 @@ class Cart:
     
     lista_produtos = []
     
-    def valor_total_carrinho(self,lista_produtos):
+    def valor_total_carrinho(self,lista_produtos=lista_produtos):
         valor_total = 0
         for item in lista_produtos:
             valor_total += item.valor
         return valor_total
+
+class Correio:
     
+    def __init__(self,cep:str) -> float:
+        self.cep = cep
+        return 10.5
+
 
--- testes.py
+++ testes.py
@@ -2,6 +2,7 @@ import unittest
 from app.exercicio_1 import Multiplos
 from app.exercicio_2 import NumerosFeliz
 from app.exercicio_3 import PalavrasEmNumeros
+from app.exercicio_4 import Usuario, Product, Cart
 
 
 class TestesExercicio1(unittest.TestCase):
@@ -37,7 +38,7 @@ class TestesExercicio2(unittest.TestCase):
         resultado_verdadeiro = NumerosFeliz.soma_quadrados_digitos(self, 22)
         teste_verdadeiro = 8
         self.assertEqual(resultado_verdadeiro, teste_verdadeiro)
-    
+
     def testando_soma_quadrados_digitos_falso(self):
         resultado_verdadeiro = NumerosFeliz.soma_quadrados_digitos(self, -22)
         teste_falso = 0
@@ -61,12 +62,14 @@ class TestesExercicio3(unittest.TestCase):
         self.assertEqual(resultado_teste, resultado_esperado)
 
     def testando_palavra_em_valor_numerico(self):
-        resultado_teste = PalavrasEmNumeros.valor_resultante_caracteres('abacate')
+        resultado_teste = PalavrasEmNumeros.valor_resultante_caracteres(
+            'abacate')
         resultado_esperado = 33
         self.assertEqual(resultado_teste, resultado_esperado)
-    
+
     def testando_retorno_numerico_quando_tem_numero_na_palavra(self):
-        resultado_teste = PalavrasEmNumeros.valor_resultante_caracteres('abacate97')
+        resultado_teste = PalavrasEmNumeros.valor_resultante_caracteres(
+            'abacate97')
         resultado_esperado = 33
         self.assertEqual(resultado_teste, resultado_esperado)
 
@@ -77,7 +80,7 @@ class TestesExercicio3(unittest.TestCase):
     def testando_se_nao_retorna_numero_primo(self):
         resultado_teste_false = PalavrasEmNumeros.verifica_numero_primo(15)
         self.assertEqual(resultado_teste_false, False)
-    
+
     def testando_se_nao_retorna_numero_primo_quando_valor_negativo(self):
         resultado_teste_negativo = PalavrasEmNumeros.verifica_numero_primo(-11)
         self.assertEqual(resultado_teste_negativo, False)
@@ -89,15 +92,40 @@ class TestesExercicio3(unittest.TestCase):
     def testando_numero_multiplo_de_tres_ou_cinco(self):
         resultado_teste = PalavrasEmNumeros.verifica_multiplos(15)
         self.assertEqual(resultado_teste, True)
-    
+
     def testanto_caracteristicas_do_numero(self):
         numero_primo = PalavrasEmNumeros.verifica_caracteristicas_numero(5)
-        self.assertEqual(numero_primo, '- Numero é primo - Numero é multiplo de 3 ou 5 ')
+        self.assertEqual(
+            numero_primo, '- Numero é primo - Numero é multiplo de 3 ou 5 ')
 
 
 class TestesExercicio4(unittest.TestCase):
-    pass
 
+    teste_usuario = Usuario('Joao', '29185000')
+    teste_produto = Product('Celular', 1500)
+    teste_carrinho = Cart(teste_usuario)
+
+    def testando_classe_usuario(self, Usuario=teste_usuario):
+        self.assertEqual(Usuario.nome, 'Joao')
+        self.assertEqual(Usuario.cep, '29185000')
+
+    def testando_classe_produto(self, Produto=teste_produto):
+        self.assertEqual(Produto.nome, 'Celular')
+        self.assertEqual(Produto.valor, 1500)
+
+    def testando_classe_carrinho(self, Cart=teste_carrinho):
+        self.assertEqual(Cart.usuario.nome, 'Joao')
+
+    def testando_retorno_valor_total_compras(self,carrinho = teste_carrinho):
+        carrinho.lista_produtos.append(Product('Celular',1500))
+        carrinho.lista_produtos.append(Product('Monitor',1000))
+        valor_total_carrinho = carrinho.valor_total_carrinho()
+        self.assertEqual(valor_total_carrinho, 2500)
+    
+    def testando_carrinho_vazio(self, carrinho = teste_carrinho):
+        valor_total_carrinho = carrinho.valor_total_carrinho()
+        self.assertEqual(valor_total_carrinho, 0)
+  
 
 if __name__ == '__main__':
     unittest.main()
